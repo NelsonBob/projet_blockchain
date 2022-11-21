ea597759cc5f93875e53880b89520526e69707f3
NelsonBob <91746270+NelsonBob@users.noreply.github.com>
2022-11-21 20:25:31 +0100

    tree component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Navbar from './components/NavBar/index';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Blogs from './components/Blogs';
 function App() {
   return (
     <>
-      <h1>Hello Word je suis Nelson</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/blogs" element={<Blogs/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
